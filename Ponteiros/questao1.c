174a269bb42788d5bc55d3909dc4fb1388dcfafd
JeffersonMelo1199 <50911346+JeffersonMelo1199@users.noreply.github.com>
2019-08-28 22:51:45 -0300

    Update questao1.c

--- Ponteiros/questao1.c
+++ Ponteiros/questao1.c
@@ -1,1 +1,2 @@
-#include <std
+//O uso de ponteiro é de tamanha importância pois garante uma maior otimização dos programas 
+//e que os problemas a serem resolvidos por um engenheiro demandará desse conhecimento em sua maioria
