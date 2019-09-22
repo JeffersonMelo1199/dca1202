3691767dec8e3d8e447fbc31f447e53d03604057
JeffersonMelo1199 <50911346+JeffersonMelo1199@users.noreply.github.com>
2019-09-22 15:47:14 -0300

    Add files via upload

---
+++ Escultor Digital/sculptor/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include "sculptor.h"
+
+
+using namespace std;
+
+int main()
+{
+    Sculptor a(90,90,90);
+    a.setColor(0.5,1.0,0.5,0.2);
+
+    a.putVoxel(5,5,5);
+    //a.cutVoxel(5,5,5);
+
+    a.putBox(10,25,10,25,10,25);
+    //a.cutBox(10,25,10,25,10,25);
+
+    a.putSphere(35,30,30,15);
+    //a.cutSphere(35,30,30,15);
+
+    a.putEllipsoid(50,35,70,22,15,17);
+    //a.cutEllipsoid(50,35,70,22,15,17);
+
+
+    char* filename;
+    filename = "C:/Users/Jefferson Melo/Desktop/Escultor Digital/a.off";
+    a.writeOFF(filename);
+
+
+    return 0;
+}
