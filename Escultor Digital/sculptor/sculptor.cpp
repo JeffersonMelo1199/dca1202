a45bed890a1d2bb3d05e28069f7a90a1095fa288
JeffersonMelo1199 <50911346+JeffersonMelo1199@users.noreply.github.com>
2019-09-22 21:07:56 -0300

    Add files via upload

--- Escultor Digital/sculptor/sculptor.cpp
+++ Escultor Digital/sculptor/sculptor.cpp
@@ -8,20 +8,21 @@ using namespace std;
 
 Sculptor::Sculptor(int _nx, int _ny, int _nz){
 nx=_nx, ny=_ny, nz=_nz;
-
+//alocação do array auxiliar de array's
 v=new Voxel**[nx];
 if( v == NULL){
     cout << "new error\n";
 }
+//alocação de outro array auxiliar salvando a posição no array de array's
 v[0]=new Voxel*[nx*ny];
 if( v[0] == NULL){
     cout << "new error\n";
 }
+//alocação da matriz 3D
 v[0][0]=new Voxel[nx*ny*nz];
 if( v[0][0] == NULL){
     cout << "new error\n";
 }
-
 for(int i=0; i< nx; i++){
     if (i<(nx-1)){
         v[i+1] = v[i]+ny;
@@ -33,6 +34,7 @@ for(int i=0; i< nx; i++){
         }
     }
 }
+// preenchimento da matriz alocada com o isOn do struct de Voxel = false
 for(int i = 0; i < nx; i++){
     for(int j = 0; j < ny; j++){
         for(int k = 0; k < nz; k++){
@@ -40,25 +42,27 @@ for(int i = 0; i < nx; i++){
         }
     }
 }
-cout<< "construtor padrao" << endl;
+cout<< "construtor" << endl;
 }
-
+// destrutor que tem como função liberar a memoria
 Sculptor::~Sculptor(){
     delete [] v[0][0];
     delete [] v[0];
     delete [] v;
     cout <<"destrutor" << endl;
 }
-
+// atribui as cores
 void Sculptor::setColor(float r, float g, float b, float alpha){
     this -> r = r;
     this -> g = g;
     this -> b = b;
     this -> a = alpha;
 }
-
+// cria um voxel
 void Sculptor::putVoxel(int x, int y, int z){
+    // verificação das dimensões atribuida.
     if(x>=0 && y>=0 && z>=0 && x<nx && y<ny && z<nz)    {
+       // atribuições das cores para o Voxel selecionado e acionando o isOn do referido voxel
         v[x][y][z].r = r;
         v[x][y][z].g = g;
         v[x][y][z].b = b;
@@ -66,15 +70,19 @@ void Sculptor::putVoxel(int x, int y, int z){
         v[x][y][z].isOn = true;
     }
 }
-
+// apagar um voxel
 void Sculptor::cutVoxel(int x, int y, int z){
+    // verificação das dimensões atribuida.
     if(x>=0 && y>=0 && z>=0 && x<nx && y<ny && z<nz){
+        // Setando o isOn do voxel selecionado para false
         v[x][y][z].isOn=false;
+
     }
 }
-
+// adiciona um cubo
 void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
     int i,j,k;
+    // verificação das dimensões atribuida.
     if(x0>=0 && y0>=0 && z0>=0 && x0<nx && y0<ny && z0<nz && x1>=0 && y1>=0 && z1>=0 && x1<nx && y1<ny && z1<nz){
         for(i = x0; i<=x1; i++){
             for(j = y0; j<=y1; j++){
@@ -89,9 +97,10 @@ void Sculptor::putBox(int x0, int x1, int y0, int y1, int z0, int z1){
         }
     }
 }
-
+// faz o corte no formato de uma cubo
 void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
     int i,j,k;
+    // verificação das dimensões atribuida.
     if(x0>=0 && y0>=0 && z0>=0 && x0<nx && y0<ny && z0<nz && x1>=0 && y1>=0 && z1>=0 && x1<nx && y1<ny && z1<nz){
         for(int i = x0; i<=x1; i++){
             for(j = y0; j<=y1; j++){
@@ -107,12 +116,13 @@ void Sculptor::cutBox(int x0, int x1, int y0, int y1, int z0, int z1){
     }
 
 }
-
+// adiciona uma esfera com base na coordenada do centro da esfera e o seu raio
 void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
     int i,j,k;
     for(i = xcenter-radius; i<xcenter+radius; i++){
         for(j = ycenter-radius; j<ycenter+radius; j++){
             for(k = zcenter-radius; k<zcenter+radius; k++){
+                // utilizando a equação da esfera para adicionar os voxel's
                 if((pow((i-xcenter),2)+pow((j-ycenter),2)+pow((k-zcenter),2))<=pow(radius,2)){
                     v[i][j][k].r = r;
                     v[i][j][k].g = g;
@@ -124,7 +134,7 @@ void Sculptor::putSphere(int xcenter, int ycenter, int zcenter, int radius){
         }
     }
 }
-
+// faz o corte no formato de uma esfera com base na coordenada do centro da esfera e o seu raio
 void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
     int i,j,k;
     for(i = xcenter-radius; i<xcenter+radius; i++){
@@ -138,7 +148,7 @@ void Sculptor::cutSphere(int xcenter, int ycenter, int zcenter, int radius){
     }
 
 }
-
+// adiciona um elipse com base na coordenada do centro e os raios
 void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
     int i,j,k;
     for(i = xcenter-rx; i<xcenter+rx; i++){
@@ -157,7 +167,7 @@ void Sculptor::putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
         }
     }
 }
-
+// faz o corte em formato de uma elipse com base na coordenada do centro e os raios
 void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz){
     int i,j,k;
     for(i = xcenter-rx; i<xcenter+rx; i++){
@@ -172,12 +182,15 @@ void Sculptor::cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int r
         }
     }
 }
-
+// gera o arquivo OFF com base nos metodos da classe sculptor utilizados
 void Sculptor::writeOFF(char* filename){
     int NV = 0, NF = 0;
     int i,j,k;
+    // variavel para efetuar as funções de fluxos de dados
     ofstream fout;
+    // abrindo o arquivo
     fout.open(filename);
+    // conta a quantidade de faces e vertices que estão com isOn verdadeiro.
     if(!fout.is_open()){
         exit(0);
     }
@@ -191,9 +204,11 @@ void Sculptor::writeOFF(char* filename){
             }
         }
     }
+    // linha de reconhecimento do tipo do arquivo
     fout<<"OFF"<<endl;
+    // numero de vertices, faces e das arestas que sempre será 0
     fout<<NV<<" "<<NF<<" "<<0<<endl;
-
+    // dimensionamento dos vertices em relação aos voxel's com isOn verdadeiro
     for(i = 0; i < nx; i++){
         for(j = 0; j < ny; j++){
             for(k = 0; k < nz; k++){
@@ -210,6 +225,7 @@ void Sculptor::writeOFF(char* filename){
             }
         }
     }
+    // dimensionamento de cada face e atribuição das cores
     int aux = 0;
     for(i = 0; i < nx; i++){
         for(j = 0; j < ny; j++){
